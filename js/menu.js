9e04682c18ca42a00189c3e0ef84b8da220d955c
wildochambia <57407682+wildochambia@users.noreply.github.com>
2020-06-07 22:44:48 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,29 @@
+let btnMenu = document.getElementById('btnbars');
+let menu = document.getElementById('bars');
+btnMenu.addEventListener('click', function() {
+    'use strict';
+    menu.classList.toggle('mostrar');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
